1e7087b123a7e206f20a9c94e18ccb13844a8796
Britt Carter <bcarter@priefert.com>
2026-07-17 15:31:47 -0500

    Updated flathub install command to be system wide

--- flathub.sh
+++ flathub.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --system --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
