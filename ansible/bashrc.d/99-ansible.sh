bcd012808ce3fe7c3ffe4916b0545b492391626e
Britt Carter <nukelheadmp@gmail.com>
2026-07-18 02:08:20 -0500

    consolidated ansible-local function with network and servers

--- ansible/bashrc.d/99-ansible.sh
+++ ansible/bashrc.d/99-ansible.sh
@@ -1,32 +1,14 @@
-ansible-local() {
-  if [[ -z "$1" ]]; then
-    echo -e "Usage: ansible-local <playbook> [options...]"
-    return 1
-  fi
-
-  if [[ "$PWD" != "${PROJECTSDIR}/ansible-servers" ]]; then
-    echo -e " -> Moving to ansible directory..."
-    cd ${PROJECTSDIR}/ansible-servers
-  fi
-
-  local playbook="playbooks/${1}"
-
-  if [[ ! -f "${playbook}" ]]; then
-    echo -e "Error: Playbook not found: ${playbook}"
-    return 1
-  fi
-
-  if [[ -z "${VIRTUAL_ENV:-}" || "${VIRTUAL_ENV}" != "${PYENV_PATH}/ansible" ]]; then
-    activate ansible
-  fi
-
-  ansible-playbook "$playbook" "${@:2}" \
-    -i "localhost," -c local \
-    --ask-become-pass
-}
-
 _ansible-passbolt() {
-  local directory="$1"
+  local localhost
+  local directory
+
+  if [[ "$1" == "local" ]]; then
+    localhost=1
+    directory="servers"
+  else
+    localhost=0
+    directory="$1"
+  fi
   shift
 
   if [[ -z "$1" ]]; then
@@ -50,11 +32,18 @@ _ansible-passbolt() {
     activate ansible
   fi
 
-  ansible-playbook "$playbook" "${@:2}" \
-    -i inventories/production/ \
-    --extra-vars @~/.ansible/vaults/vault_passbolt.yml \
-    --ask-vault-pass
+  if [[ $localhost == 1 ]]; then
+    ansible-playbook "$playbook" "${@:2}" \
+      -i "localhost," -c local \
+      --ask-become-pass
+  else
+    ansible-playbook "$playbook" "${@:2}" \
+      -i inventories/production/ \
+      --extra-vars @~/.ansible/vaults/vault_passbolt.yml \
+      --ask-vault-pass
+  fi
 }
 
+ansible-local() { _ansible-passbolt local "$@"; }
 ansible-servers() { _ansible-passbolt servers "$@"; }
 ansible-network() { _ansible-passbolt network "$@"; }
